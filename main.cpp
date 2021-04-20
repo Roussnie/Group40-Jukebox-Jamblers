348e351d0ffea28c237b0fab304749c86d793d6c
Roussnie <68291836+Roussnie@users.noreply.github.com>
2021-04-19 20:52:26 -0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@
 using namespace std;
 
 struct Song {
-    
+
     string artist;
     string name;
     int popularity = 0;
@@ -21,78 +21,78 @@ struct Song {
     vector<string> nameVect;
     vector<int> popularityVect;
     vector<int> yearVect;
-    
-    Song(){ //Default Constructor//
-        
+
+    Song() { //Default Constructor//
+
         artist = "";
         name = "";
         popularity = 0;
         releaseYear = 0;
-       
+
         count = 0;
         lines = "";
-        
+
         artistVect = {};
         nameVect = {};
         popularityVect = {};
         yearVect = {};
     }
-    
-    Song(const Song &d){ //Copy constructor//
-        
+
+    Song(const Song& d) { //Copy constructor//
+
         this->artist = d.artist;
         this->name = d.name;
         this->popularity = d.popularity;
         this->releaseYear = d.releaseYear;
-        
+
         this->count = d.count;
         this->lines = d.lines;
-        
+
         this->artistVect = d.artistVect;
         this->nameVect = d.nameVect;
         this->popularityVect = d.popularityVect;
         this->yearVect = d.yearVect;
     }
-    
-    Song &operator=(const Song &d){ //Copy assignment operator//
-        
+
+    Song& operator=(const Song& d) { //Copy assignment operator//
+
         this->artist = d.artist;
         this->name = d.name;
         this->popularity = d.popularity;
         this->releaseYear = d.releaseYear;
-        
+
         this->count = d.count;
         this->lines = d.lines;
-        
+
         this->artistVect = d.artistVect;
         this->nameVect = d.nameVect;
         this->popularityVect = d.popularityVect;
         this->yearVect = d.yearVect;
-        
+
         return *this;
     }
-    
+
     bool operator < (const Song& rhs) const { //organize set based off name in ascending order
         return name < rhs.name;
     }
 };
 
-set<Song> CreateSet(Song song, vector<int> index){
-    
+set<Song> CreateSet(Song song, vector<int> index) {
+
     set<Song> songSet;
-    
+
     for (int i = 0; i < index.size(); i++) {
-        
+
         Song temp; //make a temporary song
-        
+
         temp.artist = song.artistVect[index[i]];
         temp.name = song.nameVect[index[i]];
         temp.popularity = song.popularityVect[index[i]];
         temp.releaseYear = song.yearVect[index[i]];
-        
+
         songSet.insert(temp); //insert it into the set
     }
-    
+
     return songSet;
 }
 
@@ -100,10 +100,12 @@ set<Song> CreateSet(Song song, vector<int> index){
 int main()
 {
     Song song;
-    JJTree tree;    
+
     set<Song> songSet;
+
+    JJTree tree;
     TreeNode* root = nullptr;
-    
+
     /*======= Load data from file(s) =======*/
     ifstream file("P3_song_data csv.csv");
 
@@ -136,16 +138,16 @@ int main()
         }
     }
     file.close();
-    
+
     //*********Interface*********//
     cout << "Welcome to the Jukebox Jambler!" << endl;
-    
+
     //user enters word to search for
     string wordInput = "";
     cout << "Enter a word based on what you would like to hear: ";
     getline(cin, wordInput);
     cout << endl;
-    
+
     vector<int>index;
 
     string word = "";
@@ -164,12 +166,12 @@ int main()
         }
         word = "";
     }
-    
+
     if (index.size() == 0) {
-        cout << "Sorry, we dont have any songs matching " << wordInput << ". Goodbye. \n";
+        cout << "Sorry, we dont have any songs matching " << wordInput << ". Goodbye\n";
         return 0;
     }
-    
+
     //user chooses data structure
     cout << "Which Data Stucture would you like to use?\n";
     cout << "1. Set\n";
@@ -179,42 +181,37 @@ int main()
     string input1 = "";
     getline(cin, input1);
     cout << endl;
-    
-        cout << "We have " << index.size() << " song(s) similar to '" << wordInput << "':\n" << endl;
 
-    
-    if (input1 == "1"){
-        
+    cout << "We have " << index.size() << " song(s) similar to '" << wordInput << "':\n" << endl;
+
+    if (input1 == "1") {
+
         songSet = CreateSet(song, index); //creates the set
-        
-        for(const auto& s : songSet){ //prints set
-            
+
+        for (const auto& s : songSet) { //prints set
+
             cout << "Title: " << s.name << endl;
             cout << "Artist: " << s.artist << endl;
             cout << "Year of Release: " << s.releaseYear << endl;
             cout << "Popularity: " << s.popularity << endl;
             cout << endl;
         }
-    } 
-    else if(input1 == "2"){
-        
+    }
+    else if (input1 == "2") {
+
         for (int i = 0; i < index.size(); i++) {  //creates the tree
             tree.Insert(root, index[i], song.artistVect[index[i]], song.nameVect[index[i]], song.popularityVect[index[i]], song.yearVect[index[i]]);
         }
         tree.PrintInorder(root, root->artist, root->name, root->popularity, root->releaseYear);
-                
+
     }
     else {
         cout << "Invalid entry!\n";
     }
-    
-    
-
     return 0;
 }
 
 
 //Sources:
-//https://www.kaggle.com/yamaerenay/spotify-dataset-19212020-160k-tracks
 //https://www.geeksforgeeks.org/split-a-sentence-into-words-in-cpp/
-//Roussnie's Project 1 AVL Tree implementation 
+//Roussnie Project 1 Implementation
